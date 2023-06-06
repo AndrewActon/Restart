6a3083a29ea6a7db844ee474ca67324b0934ec09
Andrew Acton <andrew.acton.aa@gmail.com>
2023-06-06 16:28:12 -0600

    Complete

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Andrew Acton on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
